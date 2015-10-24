8333c8a766d1863ecc8b7ffb6ef8d169c953f901
Kyle Travis <kyle@kyletravis.com>
2015-10-24 15:43:33 -0500

    added null buildpack to push and parameterized domain on map-route

--- contoso-university-v2/demo2.bat
+++ contoso-university-v2/demo2.bat
@@ -1,7 +1,6 @@
-cf push contoso-demo-v2 -s windows2012R2 --no-start
+cf push contoso-demo-v2 -b https://github.com/ryandotsmith/null-buildpack.git -s windows2012R2 --no-start
 cf enable-diego contoso-demo-v2
 cf set-health-check contoso-demo-v2 none
 cf bind-service contoso-demo-v2 contoso-demo-sql
 cf bind-service contoso-demo-v2 contoso-demo-queue
-rem cf bind-service contoso-demo-v2 netlogger
 cf start contoso-demo-v2
